9a1300f7068a49415db5b456618e7170995d250f
Nicolas Meylan <nicolas.meylan@mineral.software>
2019-06-10 16:19:34 +0200

    Bump version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import TreeViewItemValue from './TreeViewItemValue.vue'
 import TreeViewItem from './TreeViewItem.vue'
 import TreeView from './TreeView.vue'
 
-module.exports = function install (Vue) {
+export default function install (Vue) {
   Vue.component('tree-view-item-value', TreeViewItemValue)
   Vue.component('tree-view-item', TreeViewItem)
   Vue.component('tree-view', TreeView)
